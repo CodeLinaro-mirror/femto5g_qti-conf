27555de9b2cec79dc9a1e41f6ad827d45983f838
Andrew Walker <andreww@codeaurora.org>
2011-10-21 14:03:02 -0700

    mv non-prop out of oe/build
    Change-Id: I4a7ef8276e3f5dc390d4683aa795f45dfdedc4f7

---
+++ make_bbimg.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+bbroot=${WORKSPACE}/build/tmp/work/armv7-none-linux-gnueabi/busybox-static-1.18.5-r43.0
+devlist=${bbroot}/geninit
+
+mkbootpath="${WORKSPACE}/build/tmp/sysroots/x86_64-linux/bin"
+kernel="${WORKSPACE}/build/tmp/work/9615-cdp-none-linux-gnueabi/linux-quic-3.0-r0/kernel"
+gencpio="${kernel}/usr/gen_init_cpio"
+
+set -x
+
+cd $bbroot
+$gencpio $devlist | gzip -n -9 > initrd.gz
+#find ${bbroot}/bb-initrd | cpio -o -H newc | gzip > ${bbroot}/bb-initrd.img.gz
+cd -
+
+output=${WORKSPACE}/boot-oe-msm9615.img
+
+kernelsize=`awk --non-decimal-data '/ _end/ {end="0x" $1} /_stext/ {beg="0x" $1} END {size1=end-beg+4096; size=and(size1,compl(4095)); printf("%#x",size)}' ${kernel}/System.map`
+
+${mkbootpath}/mkbootimg --kernel ${kernel}/arch/arm/boot/Image \
+		--ramdisk /dev/null \
+		--cmdline "noinitrd root=/dev/mtdblock1 rw rootfstype=yaffs2 console=ttyHSL0,115200,n8 no_console_suspend=1 androidboot.hardware=qcom" \
+		--base 0x40800000 \
+		--ramdisk_offset $kernelsize \
+		--output $output
+
+set +x
+
+if [ -f $output ]; then
+  echo
+  echo "Image complete: $output"
+  echo
+fi
---
+++ set_bb_env.sh
@@ -0,0 +1,42 @@
+#WORKSPACE is the parent directory of this script
+scriptdir="$(dirname "${BASH_SOURCE}")"
+export WORKSPACE=$(readlink -f $scriptdir/../..)
+WS=${WORKSPACE}
+echo WORKSPACE set to: ${WS}
+
+EXISTING_TOOLCHAIN_PATH="/afs/qualcomm.com/amd64_linux24/usr/local/packages/asw/compilers/gnu/codesourcery/arm-2009q1-203"
+DL_TOOLCHAIN_PATH="${WS}/build/tmp/work/armv7-none-linux-gnueabi/external-toolchain-csl-1.0-r11/arm-2009q1"
+
+#0 = dont download toolchain, use EXISTING_ setting
+#1 = download toolchain, use DL_ setting
+DL_TOOL="0"
+
+if [ -e ${EXISTING_TOOLCHAIN_PATH} ];then
+  export TOOLCHAIN_PATH=${EXISTING_TOOLCHAIN_PATH}
+else
+  export TOOLCHAIN_PATH=${DL_TOOLCHAIN_PATH}
+  DL_TOOL="1"
+fi
+
+export DL_TOOL
+
+CSPATH=${TOOLCHAIN_PATH}/bin
+export PATH=${CSPATH}:${PATH}
+unset BBPATH # Needed for transition to BB layers
+
+#let bb use the $WORKSPACE, $DL_TOOL, and $TOOLCHAIN_PATH var
+export BB_ENV_EXTRAWHITE="WORKSPACE DL_TOOL TOOLCHAIN_PATH"
+umask 022
+
+bitbake() {
+  pushd $WS/build
+  $WS/build/bitbake/bin/bitbake $*
+  popd
+}
+
+build9615() {
+  bitbake pkgconfig-native gtk-doc-native gettext-native external-toolchain-csl
+  bitbake glib-2.0
+  bitbake virtual/bootloader virtual/kernel mkbootimg-native 9615-cdp-image
+  $WS/build/qcom-recipes/make_bbimg.sh
+}
