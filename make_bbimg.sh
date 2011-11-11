811be5a1a2f6b42d1d9d62a2d67ed5a8cc30524e
Christopher Covington <cov@codeaurora.org>
2011-11-11 17:37:41 -0500

    make_bbimg.sh: Use new kernel paths
    Also remove some unneeded lines
    
    Change-Id: Icccbd93e3c34212d3c038369727e25d5067ea49a

--- make_bbimg.sh
+++ make_bbimg.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-bbroot=${WORKSPACE}/build/tmp/work/armv7-none-linux-gnueabi/busybox-static-1.18.5-r43.0
-devlist=${bbroot}/geninit
-
 mkbootpath="${WORKSPACE}/build/tmp/sysroots/x86_64-linux/bin"
-kernel="${WORKSPACE}/build/tmp/work/9615-cdp-none-linux-gnueabi/linux-quic-3.0-r0/kernel"
+sysroot="${WORKSPACE}/build/tmp/sysroots/armv7-none-linux-gnueabi"
+ver=`sed -r 's/#define UTS_RELEASE "(.*)"/\1/' ${sysroot}/usr/src/linux/include/generated/utsrelease.h`
 
 set -x
 
 output=${WORKSPACE}/build/tmp/deploy/images/9615-cdp/boot-oe-msm9615.img
 
-kernelsize=`awk --non-decimal-data '/ _end/ {end="0x" $1} /_stext/ {beg="0x" $1} END {size1=end-beg+4096; size=and(size1,compl(4095)); printf("%#x",size)}' ${kernel}/System.map`
+kernelsize=`awk --non-decimal-data '/ _end/ {end="0x" $1} /_stext/ {beg="0x" $1} END {size1=end-beg+4096; size=and(size1,compl(4095)); printf("%#x",size)}' ${sysroot}/boot/System.map-${ver}`
 
-${mkbootpath}/mkbootimg --kernel ${kernel}/arch/arm/boot/Image \
+${mkbootpath}/mkbootimg --kernel ${sysroot}/boot/Image-${ver} \
 		--ramdisk /dev/null \
 		--cmdline "noinitrd root=/dev/mtdblock9 rw rootfstype=yaffs2 console=ttyHSL0,115200,n8 no_console_suspend=1 androidboot.hardware=qcom" \
 		--base 0x40800000 \
