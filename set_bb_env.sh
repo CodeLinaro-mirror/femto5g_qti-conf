fae17b24ac55c81ab06d293f0ad3025fe70ef268
S A Muqthyar <muqthyar@codeaurora.org>
2018-07-03 12:35:04 +0530

    QCS605: Add Build command for Both 64 and 32 bit machines
    Added Commands
    QCS605-32:
    	build-qcs605-32-concam-perf-image
    	build-qcs605-32-concam-image
    	build-all-qcs605-32-concam-images
    
    QCS605-64:
    	build-qcs605-64-concam-perf-image
    	build-qcs605-64-concam-image
    	build-all-qcs605-64-concam-images
    
    Change-Id: I8af2b2d34007b81fcbf6dc33628cf1091d2b495f

--- set_bb_env.sh
+++ set_bb_env.sh
@@ -500,6 +500,48 @@ build-all-qcs605-concam-images() {
   build-qcs605-concam-perf-image
 }
 
+# qcs605-32 commands
+function build-qcs605-32-concam-perf-image() {
+  unset_bb_env
+  export MACHINE=qcs605-32
+  export DISTRO=concam
+  export VARIANT=perf
+  cdbitbake machine-image
+}
+
+function build-qcs605-32-concam-image() {
+  unset_bb_env
+  export MACHINE=qcs605-32
+  export DISTRO=concam
+  cdbitbake machine-image
+}
+
+build-all-qcs605-32-concam-images() {
+  build-qcs605-32-concam-image
+  build-qcs605-32-concam-perf-image
+}
+
+# qcs605-64 commands
+function build-qcs605-64-concam-perf-image() {
+  unset_bb_env
+  export MACHINE=qcs605-64
+  export DISTRO=concam
+  export VARIANT=perf
+  cdbitbake machine-image
+}
+
+function build-qcs605-64-concam-image() {
+  unset_bb_env
+  export MACHINE=qcs605-64
+  export DISTRO=concam
+  cdbitbake machine-image
+}
+
+build-all-qcs605-64-concam-images() {
+  build-qcs605-64-concam-image
+  build-qcs605-64-concam-perf-image
+}
+
 # Utility commands
 buildclean-retaindeploy() {
   set -x
