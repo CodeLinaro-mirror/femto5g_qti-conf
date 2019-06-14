eb7518abb317dcef52d3abc518f2cd6432be2414
Nan Zhang <nanzha@codeaurora.org>
2019-06-14 11:19:40 +0800

    Enabled the meta-gplv2 layer for sa8155ivi.
    Change-Id: I48d9cb123721389a381c1698ea87a089610e53bf
    Signed-off-by: Nan Zhang <nanzha@codeaurora.org>
    Signed-off-by: Yingzhong.Zha <yzha@codeaurora.org>

--- layers.py
+++ layers.py
@@ -32,6 +32,7 @@ def initLayersList(MACHINE):
     dicLayersWithSubLayers = { \
         "poky": { "meta":1, "meta-poky":1 }, \
         "meta-qt5": 1, \
+        "meta-gplv2": 1, \
         "meta-qti-internal": 1, \
         "meta-openembedded": { "meta-networking":1, "meta-python":1, "meta-oe":1, "meta-filesystems":1, "meta-multimedia":1 }, \
         "meta-qti-bsp-prop": {"meta-qti-base-prop":1, "meta-qti-extra-prop":1, "meta-qti-qtee":1 }, \
