f9443176343f9fe149de20f71234b0df01188fb4
Liuliu Zhao <liulzhao@codeaurora.org>
2019-10-22 12:18:16 +0800

    qti-conf: Remove BT recipes and denpendencies from ignore list
    Enable meta-qti-bt and meta-qti-bt-prop in generated bblayer.conf.
    Some denpendencies of BT are also removed from ignore list in
    get_bblayers.
    
    Change-Id: I0693d58e0cc5109fd43c70a1f2c47d050d59ca88

--- get_bblayers.py
+++ get_bblayers.py
@@ -20,10 +20,7 @@ def initLayersList(target):
                     "meta-qti-audio", "meta-qti-audio-cal", \
                     "meta-qti-audio-external", "meta-qti-audio-prop", \
                     "meta-qti-avs", "meta-qti-avs-prop", \
-                    "meta-qti-bt", \
-                    "meta-qti-bt-prop", "meta-qti-camera", \
-                    "meta-qti-camera-prop", \
-                    "meta-qti-data-prop", \
+                    "meta-qti-camera", "meta-qti-camera-prop" \
                     "meta-qti-display-prop", "meta-qti-display", \
                     "meta-qti-drones", "meta-qti-drones-prop", \
                     "meta-qti-gfx-prop", "meta-qti-gst", \
