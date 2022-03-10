cf8af6c65c6820a4f9732f6d26e0c2f8a430c715
Shangjun Yue <quic_syue@quicinc.com>
2022-03-10 10:21:01 +0800

    layers.py: Enable meta-clang layer to support LXC function compilation on sa81x5
    Change-Id: Ifa6b730d1ccf6f8ec3aa013d4227cdd1adb7e753
    Signed-off-by: Shangjun Yue <quic_syue@quicinc.com>

--- layers.py
+++ layers.py
@@ -50,7 +50,7 @@ def initLayersList(TARGET):
         "meta-security": 1 \
     }
 
-    if TARGET == 'sa6155' or TARGET == 'sa81x5' or TARGET == 'sa81x5-rt' or TARGET == 'sa8295':
+    if TARGET == 'sa6155' or TARGET == 'sa81x5' or TARGET == 'sa81x5-rt' or TARGET == 'sa8295' or TARGET == 'sa81x5lxc':
         del dicLayersWithSubLayers["meta-qt5"]
         # Enable upsteam llvm
         dicLayersWithSubLayers["meta-qti-bsp"]["meta-qti-clang"] = 1
@@ -85,8 +85,3 @@ def initLayersList(TARGET):
         dicLayersWithSubLayers["meta-qti-bsp"]["meta-qti-clang"] = 1
         dicLayersWithSubLayers["meta-qti-bsp-prop"]["meta-qti-clang-prop"] = 1
         dicLayersWithSubLayers["meta-clang"] = 1
-    elif TARGET == 'sa6155lxc' or TARGET == 'sa81x5lxc':
-        # Enable sdllvm
-        dicLayersWithSubLayers["meta-qti-bsp"]["meta-qti-sdllvm"] = 1
-        dicLayersWithSubLayers["meta-qti-bsp-prop"]["meta-qti-sdllvm-prop"] = 1
-
