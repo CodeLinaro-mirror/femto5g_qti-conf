ab09b6c6f0e42236da5e6c9d07223caaae3c324e
Zhu Yuqing <quic_zyuqing@quicinc.com>
2022-07-26 09:42:12 +0800

    set_bb_env.sh: Update quin-gvm-gen4-dpk image folder
    Change-Id: Ife32e5e91b7a0b9d68a15dc8c657a3453379b117
    Signed-off-by: Zhu Yuqing <quic_zyuqing@quicinc.com>

--- set_bb_env.sh
+++ set_bb_env.sh
@@ -1090,13 +1090,13 @@ build-all-quin-gvm-gen4-dpk-image() {
     echo "==== Function: $FUNCNAME (${FUNCNAME[@]})"
     build-quin-gvm-gen4-dpk-image
     if [ "$?" != "0" ]; then
-    export MACHINE_IMAGE=`readlink tmp-glibc/deploy/images/quin-gvm-gen4-automotive/machine-image-quin-gvm-gen4.ext4`
-    rm -f tmp-glibc/deploy/images/quin-gvm-gen4-automotive/machine-image-quin-gvm-gen4.ext4
+    export MACHINE_IMAGE=`readlink tmp-glibc/deploy/images/quin-gvm-gen4-dpk-automotive/machine-image-quin-gvm-gen4-dpk.ext4`
+    rm -f tmp-glibc/deploy/images/quin-gvm-gen4-dpk-automotive/machine-image-quin-gvm-gen4-dpk.ext4
     echo "==== Error run 'build-quin-gvm-gen4-dpk-image'. (${FUNCNAME[@]})"
     return 1
     fi
-    export MACHINE_IMAGE=`readlink tmp-glibc/deploy/images/quin-gvm-gen4-automotive/machine-image-quin-gvm-gen4.ext4`
-    rm -f tmp-glibc/deploy/images/quin-gvm-gen4-automotive/machine-image-quin-gvm-gen4.ext4
+    export MACHINE_IMAGE=`readlink tmp-glibc/deploy/images/quin-gvm-gen4-dpk-automotive/machine-image-quin-gvm-gen4-dpk.ext4`
+    rm -f tmp-glibc/deploy/images/quin-gvm-gen4-dpk-automotive/machine-image-quin-gvm-gen4-dpk.ext4
 
     build-quin-gvm-gen4-dpk-sdk-image
     if [ "$?" != "0" ]; then
@@ -1104,19 +1104,19 @@ build-all-quin-gvm-gen4-dpk-image() {
     return 1
     fi
 
-    mv tmp-glibc/deploy/images/quin-gvm-gen4-automotive tmp-glibc/deploy/images/quin-gvm-gen4-automotive.bak
+    mv tmp-glibc/deploy/images/quin-gvm-gen4-dpk-automotive tmp-glibc/deploy/images/quin-gvm-gen4-dpk-automotive.bak
     bitbake virtual/kernel -fc cleanall
     build-quin-gvm-gen4-dpk-perf-image
     if [ "$?" != "0" ]; then
     echo "==== Error run 'build-quin-gvm-gen4-dpk-perf-image'. (${FUNCNAME[@]})"
     return 1
     fi
-    export MACHINE_IMAGE_PERF=`readlink tmp-glibc/deploy/images/quin-gvm-gen4-automotive-perf/machine-image-quin-gvm-gen4.ext4`
-    rm -f tmp-glibc/deploy/images/quin-gvm-gen4-automotive-perf/machine-image-quin-gvm-gen4.ext4
-    mv tmp-glibc/deploy/images/quin-gvm-gen4-automotive.bak tmp-glibc/deploy/images/quin-gvm-gen4-automotive
+    export MACHINE_IMAGE_PERF=`readlink tmp-glibc/deploy/images/quin-gvm-gen4-dpk-automotive-perf/machine-image-quin-gvm-gen4-dpk.ext4`
+    rm -f tmp-glibc/deploy/images/quin-gvm-gen4-dpk-automotive-perf/machine-image-quin-gvm-gen4-dpk.ext4
+    mv tmp-glibc/deploy/images/quin-gvm-gen4-dpk-automotive.bak tmp-glibc/deploy/images/quin-gvm-gen4-dpk-automotive
 
-    mv tmp-glibc/deploy/images/quin-gvm-gen4-automotive/$MACHINE_IMAGE tmp-glibc/deploy/images/quin-gvm-gen4-automotive/machine-image-quin-gvm-gen4.ext4
-    mv tmp-glibc/deploy/images/quin-gvm-gen4-automotive-perf/$MACHINE_IMAGE_PERF tmp-glibc/deploy/images/quin-gvm-gen4-automotive-perf/machine-image-quin-gvm-gen4.ext4
+    mv tmp-glibc/deploy/images/quin-gvm-gen4-dpk-automotive/$MACHINE_IMAGE tmp-glibc/deploy/images/quin-gvm-gen4-dpk-automotive/machine-image-quin-gvm-gen4-dpk.ext4
+    mv tmp-glibc/deploy/images/quin-gvm-gen4-dpk-automotive-perf/$MACHINE_IMAGE_PERF tmp-glibc/deploy/images/quin-gvm-gen4-dpk-automotive-perf/machine-image-quin-gvm-gen4-dpk.ext4
 }
 
 function build-quin-gvm-gen4-dpk-sdk-image() {
