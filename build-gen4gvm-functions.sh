8c56fd9fff51d97a8f9201515d49d50f60db0ddb
pengy <quic_pengy@quicinc.com>
2023-11-13 09:56:21 +0800

    build-gen4gvm-functions.sh: Add tgvm vmlinux in quin-gvm-gen4-2
    Change-Id: I56b1460e76be4b31ae9f7b27df70a78cf218b752
    Signed-off-by: pengy <quic_pengy@quicinc.com>

--- build-gen4gvm-functions.sh
+++ build-gen4gvm-functions.sh
@@ -729,8 +729,10 @@ build-all-quin-gvm-gen4-2-image() {
     mv tmp-glibc/deploy/images/quin-tgvm-gen4-headless-automotive-perf/$HEADLESS_IMAGE_PERF tmp-glibc/deploy/images/quin-tgvm-gen4-headless-automotive-perf/qti-image-headless-quin-tgvm-gen4-headless.ext4
     cp tmp-glibc/deploy/images/quin-tgvm-gen4-headless-automotive/qti-image-headless* tmp-glibc/deploy/images/quin-gvm-gen4-2-automotive/
     cp tmp-glibc/deploy/images/quin-tgvm-gen4-headless-automotive/quin-tgvm-gen4-headless* tmp-glibc/deploy/images/quin-gvm-gen4-2-automotive/
+    cp tmp-glibc/deploy/images/quin-tgvm-gen4-headless-automotive/vmlinux tmp-glibc/deploy/images/quin-gvm-gen4-2-automotive/quin-tgvm-gen4-headless-vmlinux
     cp tmp-glibc/deploy/images/quin-tgvm-gen4-headless-automotive-perf/qti-image-headless* tmp-glibc/deploy/images/quin-gvm-gen4-2-automotive-perf/
     cp tmp-glibc/deploy/images/quin-tgvm-gen4-headless-automotive-perf/quin-tgvm-gen4-headless* tmp-glibc/deploy/images/quin-gvm-gen4-2-automotive-perf/
+    cp tmp-glibc/deploy/images/quin-tgvm-gen4-headless-automotive-perf/vmlinux tmp-glibc/deploy/images/quin-gvm-gen4-2-automotive-perf/quin-tgvm-gen4-headless-vmlinux
 }
 
 function build-quin-gvm-gen4-2-sdk-image() {
