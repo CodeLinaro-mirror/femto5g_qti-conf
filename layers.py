8416508ee06b9380a701ea44e651828e034d58d5
Haiyi Xu <quic_haiyix@quicinc.com>
2023-11-24 16:31:36 +0800

    qti-conf: Enable QT layer on sa8775
    Change-Id: Icc95fdef847adc9cecd46836604d7d223b491890
    Signed-off-by: Haiyi Xu <quic_haiyix@quicinc.com>

--- layers.py
+++ layers.py
@@ -110,11 +110,13 @@ def initLayersList(TARGET):
         dicLayersWithSubLayers.update(dicAglDemoLayersWithSubLayers)
         # Enable upsteam llvm
         dicLayersWithSubLayers["meta-clang"] = 1
-    elif TARGET in ['sa8540', 'sa8775']:
+    elif TARGET in ['sa8775']:
+        # Enable upsteam llvm
+        dicLayersWithSubLayers["meta-clang"] = 1
+        dicLayersWithSubLayers["meta-rust"] = 1
+    elif TARGET in ['sa8540']:
         del dicLayersWithSubLayers["meta-qt5"]
         # Enable upsteam llvm
-        dicLayersWithSubLayers["meta-qti-bsp"]["meta-qti-clang"] = 1
-        dicLayersWithSubLayers["meta-qti-bsp-prop"]["meta-qti-clang-prop"] = 1
         dicLayersWithSubLayers["meta-clang"] = 1
         dicLayersWithSubLayers["meta-rust"] = 1
     elif TARGET in ['monaco']:
