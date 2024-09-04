6729b6a4eda9519f6e754b7e40378e4d2645292b
pengy <quic_pengy@quicinc.com>
2024-09-04 15:25:05 +0800

    layers.py: Del meta-gplv2 in Yocto 5.0
    Change-Id: I5796daea89f6a691a0f6c33dd60db4669a03cb52
    Signed-off-by: pengy <quic_pengy@quicinc.com>

--- layers.py
+++ layers.py
@@ -38,7 +38,6 @@ def initLayersList(TARGET):
     dicLayersWithSubLayers = { \
         "poky": { "meta":1, "meta-poky":1 }, \
         "meta-qt5": 1, \
-        "meta-gplv2": 1, \
         "meta-openembedded": { "meta-networking":1, "meta-python":1, "meta-oe":1, "meta-filesystems":1, "meta-multimedia":1, "meta-perl":1 }, \
         "meta-qti-bsp-prop": {"meta-qti-base-prop":1, "meta-qti-extra-prop":1 }, \
         "meta-qti-bsp": {"meta-qti-base":1 , "meta-qti-extra":1, "meta-qti-upstream":1, "meta-qti-distro":1 }, \
