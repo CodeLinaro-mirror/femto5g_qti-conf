43ac9dda2451f6192a304dc0f2ee9fab1014be04
Yimin Peng <yiminp@qti.qualcomm.com>
2026-05-07 16:11:56 +0530

    qti-conf: Remove bazel kernel build step for gen5gvm
    Change-Id: I709e7a5dacc2a21ce62600e6a3dfab0b92099511
    Signed-off-by: Yimin Peng <yiminp@qti.qualcomm.com>
    (cherry picked from commit bfd83a50e8c7cfef5ada0f76675c727d272fd464)

--- build-gen5gvm-functions.sh
+++ build-gen5gvm-functions.sh
@@ -3,18 +3,6 @@
 
 # gvm-gen5 commands
 function build-gvm-gen5-image() {
-  KERNEL_VARIANT="debug_defconfig"
-  KERNEL_BUILDCMD="./build_with_bazel.py -t autogvm debug-defconfig"
-  echo "building kernel"
-  cd ${WS}/kernel/kernel-6.*/kernel_platform
-  $KERNEL_BUILDCMD
-  find out/bazel -type d -exec chmod 0755 {} +
-  if [ ! -f out/msm-kernel-autogvm-$KERNEL_VARIANT/dist/Image ]; then
-      echo "Kernel compilation failed !!"
-      exit 1
-  fi
-  cd ${WS}/poky/build
-
   echo "==== Function: $FUNCNAME (${FUNCNAME[@]})"
   unset_bb_env
   init-configure-files gvm-gen5 debug
@@ -28,25 +16,9 @@ function build-gvm-gen5-image() {
   echo "==== Error run 'cdbitbake machine-image'. (${FUNCNAME[@]})"
   return 1
   fi
-
-  cd ${WS}/kernel/kernel-6.*/kernel_platform
-  rm -rf bazel-cache
-  cd ${WS}/poky/build
 }
 
 function build-gvm-gen5-perf-image() {
-  KERNEL_VARIANT="defconfig"
-  KERNEL_BUILDCMD="./build_with_bazel.py -t autogvm defconfig"
-  echo "building kernel"
-  cd ${WS}/kernel/kernel-6.*/kernel_platform
-  $KERNEL_BUILDCMD
-  find out/bazel -type d -exec chmod 0755 {} +
-  if [ ! -f out/msm-kernel-autogvm-$KERNEL_VARIANT/dist/Image ]; then
-      echo "Kernel compilation failed !!"
-      exit 1
-  fi
-  cd ${WS}/poky/build
-
   echo "==== Function: $FUNCNAME (${FUNCNAME[@]})"
   unset_bb_env
   init-configure-files gvm-gen5 perf
@@ -55,10 +27,6 @@ function build-gvm-gen5-perf-image() {
   echo "==== Error run 'cdbitbake machine-image'. (${FUNCNAME[@]})"
   return 1
   fi
-
-  cd ${WS}/kernel/kernel-6.*/kernel_platform
-  rm -rf bazel-cache
-  cd ${WS}/poky/build
 }
 
 build-all-gvm-gen5-image() {
