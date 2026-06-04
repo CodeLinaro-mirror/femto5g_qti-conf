5b771e3cf39c55c8daf75fb144b11d454624d926
pengy <pengy@qti.qualcomm.com>
2026-06-04 10:33:05 +0800

    build-gen5gvm-functions.sh: Add new build target for qclinux-gvm-gen5
    Change-Id: I75efbdd0a5a031cb21be2ba08dbf4bbf7b741005

--- build-gen5gvm-functions.sh
+++ build-gen5gvm-functions.sh
@@ -106,3 +106,83 @@ function build-gvm-gen5-sdk-image() {
     return 1
     fi
 }
+
+# qclinux-gvm-gen5 commands
+function build-qclinux-gvm-gen5-image() {
+
+  echo "==== Function: $FUNCNAME (${FUNCNAME[@]})"
+  unset_bb_env
+  cd ${WSQC}/poky
+  source build/conf/set_bb_env.sh -t qclinux-gvm-gen5 -d auto-gvm -b build -v debug
+  if [ "$?" != "0" ]; then
+  echo "==== Error run 'init-configure-files qclinux-gvm-gen5 debug'. (${FUNCNAME[@]})"
+  return 1
+  fi
+
+  bitbake machine-image
+  if [ "$?" != "0" ]; then
+  echo "==== Error run 'cdbitbake machine-image'. (${FUNCNAME[@]})"
+  return 1
+  fi
+
+}
+
+function build-qclinux-gvm-gen5-perf-image() {
+
+  echo "==== Function: $FUNCNAME (${FUNCNAME[@]})"
+  unset_bb_env
+  cd ${WSQC}/poky
+  source build/conf/set_bb_env.sh -t qclinux-gvn-gen5 -d auto-gvm -b build -v perf
+  bitbake machine-image
+  if [ "$?" != "0" ]; then
+  echo "==== Error run 'cdbitbake machine-image'. (${FUNCNAME[@]})"
+  return 1
+  fi
+
+}
+
+build-all-qclinux-gvm-gen5-image() {
+
+    echo "==== Function: $FUNCNAME (${FUNCNAME[@]})"
+    build-qclinux-gvm-gen5-image
+    if [ "$?" != "0" ]; then
+    export MACHINE_IMAGE=`readlink tmp-glibc/deploy/images/qclinux-gvm-gen5-automotive/machine-image-qclinux-gvm-gen5.ext4`
+    rm -f tmp-glibc/deploy/images/qclinux-gvm-gen5-automotive/machine-image-qclinux-gvm-gen5.ext4
+    echo "==== Error run 'build-qclinux-gvm-gen5-image'. (${FUNCNAME[@]})"
+    return 1
+    fi
+    export MACHINE_IMAGE=`readlink tmp-glibc/deploy/images/qclinux-gvm-gen5-automotive/machine-image-qclinux-gvm-gen5.ext4`
+    rm -f tmp-glibc/deploy/images/qclinux-gvm-gen5-automotive/machine-image-qclinux-gvm-gen5.ext4
+
+    #build-qclinux-gvm-gen5-sdk-image
+    #if [ "$?" != "0" ]; then
+    #echo "==== Error run 'build-qclinux-gvm-gen5-sdk-image'. (${FUNCNAME[@]})"
+    #return 1
+    #fi
+
+    mv tmp-glibc/deploy/images/qclnux-gvm-gen5-automotive tmp-glibc/deploy/images/qclinux-gvm-gen5-automotive.bak
+    bitbake virtual/kernel -fc cleanall
+    build-qclinux-gvm-gen5-perf-image
+    if [ "$?" != "0" ]; then
+    echo "==== Error run 'build-qclinux-gvm-gen5-perf-image'. (${FUNCNAME[@]})"
+    return 1
+    fi
+    export MACHINE_IMAGE_PERF=`readlink tmp-glibc/deploy/images/qclinux-gvm-gen5-automotive-perf/machine-image-qclinux-gvm-gen5.ext4`
+    rm -f tmp-glibc/deploy/images/qclinux-gvm-gen5-automotive-perf/machine-image-qclinux-gvm-gen5.ext4
+    mv tmp-glibc/deploy/images/qclinux-gvm-gen5-automotive.bak tmp-glibc/deploy/images/qclinux-gvm-gen5-automotive
+
+    mv tmp-glibc/deploy/images/qclinux-gvm-gen5-automotive/$MACHINE_IMAGE tmp-glibc/deploy/images/qclinux-gvm-gen5-automotive/machine-image-qclinux-gvm-gen5.ext4
+    mv tmp-glibc/deploy/images/qclinux-gvm-gen5-automotive-perf/$MACHINE_IMAGE_PERF tmp-glibc/deploy/images/qclinux-gvm-gen5-automotive-perf/machine-image-qclinux-gvm-gen5.ext4
+}
+
+function build-qclinux-gvm-gen5-sdk-image() {
+    echo "==== Function: $FUNCNAME (${FUNCNAME[@]})"
+    unset_bb_env
+    cd ${WSQC}/poky
+    source build/conf/set_bb_env.sh -t qclinux-gvn-gen5 -d auto-gvm -b build -v debug
+    cdbitbake machine-image -c populate_sdk
+    if [ "$?" != "0" ]; then
+    echo "==== Error run 'cdbitbake machine-image -c populate_sdk'. (${FUNCNAME[@]})"
+    return 1
+    fi
+}
