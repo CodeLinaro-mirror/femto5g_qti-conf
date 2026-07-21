4519c190a4e210976b28db6694300d866e76d2db
pengy <pengy@qti.qualcomm.com>
2026-07-21 18:04:52 +0800

    build-gen5gvm-functions.sh: Fix spelling errors
    Change-Id: Icd23b446d76c9fdd92007a661bae5d938f024b93

--- build-gen5gvm-functions.sh
+++ build-gen5gvm-functions.sh
@@ -24,7 +24,7 @@ function build-gvm-gen5-perf-image() {
   echo "==== Function: $FUNCNAME (${FUNCNAME[@]})"
   unset_bb_env
   cd ${WSQC}/poky
-  source build/conf/set_bb_env.sh -t gvn-gen5 -d auto-gvm -b build -v perf
+  source build/conf/set_bb_env.sh -t gvm-gen5 -d auto-gvm -b build -v perf
   bitbake machine-image
   if [ "$?" != "0" ]; then
   echo "==== Error run 'bitbake machine-image'. (${FUNCNAME[@]})"
@@ -71,7 +71,7 @@ function build-gvm-gen5-sdk-image() {
     echo "==== Function: $FUNCNAME (${FUNCNAME[@]})"
     unset_bb_env
     cd ${WSQC}/poky
-    source build/conf/set_bb_env.sh -t gvn-gen5 -d auto-gvm -b build -v debug
+    source build/conf/set_bb_env.sh -t gvm-gen5 -d auto-gvm -b build -v debug
     bitbake machine-image -c populate_sdk
     if [ "$?" != "0" ]; then
     echo "==== Error run 'bitbake machine-image -c populate_sdk'. (${FUNCNAME[@]})"
@@ -155,7 +155,7 @@ function build-qclinux-gvm-gen5-sdk-image() {
     echo "==== Function: $FUNCNAME (${FUNCNAME[@]})"
     unset_bb_env
     cd ${WSQC}/poky
-    source build/conf/set_bb_env.sh -t qclinux-gvn-gen5 -d auto-gvm -b build -v debug
+    source build/conf/set_bb_env.sh -t qclinux-gvm-gen5 -d auto-gvm -b build -v debug
     bitbake machine-image -c populate_sdk
     if [ "$?" != "0" ]; then
     echo "==== Error run 'bitbake machine-image -c populate_sdk'. (${FUNCNAME[@]})"
